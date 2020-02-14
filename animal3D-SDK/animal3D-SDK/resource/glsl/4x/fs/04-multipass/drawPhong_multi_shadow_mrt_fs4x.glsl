4fe005086c273adeb623a6073a13a0020701c3bb
Schnorrenberg <laura.schnorrenberg@champlain.edu>
2020-02-13 19:33:46 -0500

    Got most stuff done except outlines

--- animal3D-SDK/animal3D-SDK/resource/glsl/4x/fs/04-multipass/drawPhong_multi_shadow_mrt_fs4x.glsl
+++ animal3D-SDK/animal3D-SDK/resource/glsl/4x/fs/04-multipass/drawPhong_multi_shadow_mrt_fs4x.glsl
@@ -32,10 +32,96 @@
 //	3) declare shadow map texture
 //	4) perform shadow test
 
-out vec4 rtFragColor;
+in vec4 oMVNormie;
+in vec4 oVSPos; 
+in vec4 oTexCoord;
+in vec4 oShadowCoord;
+
+uniform sampler2D uTex_dm;
+uniform sampler2D uTex_sm;
+uniform int uLightCt;
+uniform float uLightSz;
+uniform float uLightSzInvSq;
+uniform vec4 uLightPos[4];
+uniform vec4 uLightCol[4];
+
+// lab 4
+uniform sampler2D uTex_shadow;
+
+// lab 3
+layout (location = 0) out vec4 rtFragColor;
+layout (location = 1) out vec4 rtPosition;
+layout (location = 2) out vec4 rtNormal;
+layout (location = 3) out vec4 rtTexCoord;
+layout (location = 4) out vec4 rtDiffuseTex;
+layout (location = 5) out vec4 rtSpecularTex;
+layout (location = 6) out vec4 rtDiffuseLight;
+layout (location = 7) out vec4 rtSpecularLight;
+
+float diffuse(vec4 n, vec4 l, vec4 pos);
+float specular(vec4 viewer, vec4 pos, vec4 n, vec4 l, float shinyConstant);
 
 void main()
 {
 	// DUMMY OUTPUT: all fragments are OPAQUE GREEN
-	rtFragColor = vec4(0.0, 1.0, 0.0, 1.0);
+	// phong = diffuse + specular + ambient;
+	vec4 normalizedN = normalize(oMVNormie);
+	vec4 phong  = vec4(0.0,0.0,0.0, 0.0);
+	vec4 diffuseColor = vec4(0.0, 0.0, 0.0, 1.0);
+	vec4 specularColor = vec4(0.0, 0.0, 0.0, 1.0);
+	vec4 diffuseTex;
+	vec4 specularTex;
+
+	for(int i = 0; i  < uLightCt; i++)
+	{
+		// Diffuse color and diffuse texture
+		diffuseColor += diffuse(normalizedN, uLightPos[i], oVSPos) * uLightCol[i];
+		// Specular color and specular texture
+		specularColor += specular(vec4(0.0, 0.0, 0.0, 0.0), oVSPos, normalizedN, uLightPos[i], 1.0) * uLightCol[i];
+	}
+
+	diffuseTex = diffuseColor * texture(uTex_dm, oTexCoord.xy);
+	specularTex = specularColor * texture(uTex_sm, oTexCoord.xy);
+
+	phong  = diffuseTex + specularTex;
+
+	// lab 4
+	vec4 perspecDivide = oShadowCoord / oShadowCoord.w;
+	float shadowDepth = texture(uTex_shadow, perspecDivide.xy).r;
+
+	if(phong.z > (shadowDepth + .0025))
+	{
+		phong *= 0.2;
+	}
+
+
+	rtFragColor = phong;
+
+	// lab 3
+	rtPosition = oVSPos;
+	rtNormal = normalizedN;
+	rtTexCoord = oTexCoord;
+	rtDiffuseTex = texture(uTex_dm, oTexCoord.xy);
+	rtSpecularTex = texture(uTex_sm, oTexCoord.xy);
+	rtDiffuseLight = diffuseColor;
+	rtSpecularLight = specularColor;
+}
+
+
+float diffuse(vec4 n, vec4 l, vec4 pos)
+{
+	vec4 normalizedL = normalize(l-pos);
+	float dotPro = dot(n, normalizedL);
+	
+	return dotPro;
+}
+
+float specular(vec4 viewerPos, vec4 pos, vec4 n, vec4 l, float shinyConstant)
+{
+	vec4 normalizedViewer = normalize(viewerPos - pos);
+	vec4 normalizedLight = normalize(l-pos);
+	vec4 normalizedReflection = 2 * diffuse(n, l, pos) * n - normalizedLight;
+	float specular = pow(dot(normalizedViewer, normalizedReflection), shinyConstant); //we dont know what the shiny constant should be (what uniform)
+
+	return specular;
 }
--- animal3D-SDK/animal3D-SDK/source/animal3D-DemoPlugin/A3_DEMO/a3_DemoState/a3_DemoState_loading.c
+++ animal3D-SDK/animal3D-SDK/source/animal3D-DemoPlugin/A3_DEMO/a3_DemoState/a3_DemoState_loading.c
@@ -455,7 +455,7 @@ void a3demo_loadShaders(a3_DemoState *demoState)
 			{ { { 0 },	"shdr-vs:passthru-trans-inst",		a3shader_vertex  ,	1,{ A3_DEMO_VS"e/passthru_transform_instanced_vs4x.glsl" } } },
 			{ { { 0 },	"shdr-vs:pass-col-trans-inst",		a3shader_vertex  ,	1,{ A3_DEMO_VS"e/passColor_transform_instanced_vs4x.glsl" } } },
 			// 02-shading
-			{ { { 0 },	"shdr-vs:pass-tex-trans",			a3shader_vertex  ,	1,{ A3_DEMO_VS"02-shading/e/passTexcoord_transform_vs4x.glsl" } } },
+			{ { { 0 },	"shdr-vs:pass-tex-trans",			a3shader_vertex  ,	1,{ A3_DEMO_VS"02-shading/passTexcoord_transform_vs4x.glsl" } } },
 			{ { { 0 },	"shdr-vs:pass-light-trans",			a3shader_vertex  ,	1,{ A3_DEMO_VS"02-shading/e/passLightingData_transform_vs4x.glsl" } } },
 			// 04-multipass
 			{ { { 0 },	"shdr-vs:pass-light-shadow-trans",	a3shader_vertex  ,	1,{ A3_DEMO_VS"04-multipass/passLightingData_shadowCoord_transform_vs4x.glsl" } } },
@@ -477,7 +477,7 @@ void a3demo_loadShaders(a3_DemoState *demoState)
 			{ { { 0 },	"shdr-fs:draw-Phong-multi-mrt",		a3shader_fragment,	1,{ A3_DEMO_FS"03-framebuffer/e/drawPhong_multi_mrt_fs4x.glsl" } } },
 			{ { { 0 },	"shdr-fs:draw-nonphoto-multi-mrt",	a3shader_fragment,	1,{ A3_DEMO_FS"03-framebuffer/e/drawNonphoto_multi_mrt_fs4x.glsl" } } },
 			// 04-multipass
-			{ { { 0 },	"shdr-fs:draw-tex-outline",			a3shader_fragment,	1,{ A3_DEMO_FS"04-multipass/e/drawTexture_outline_fs4x.glsl" } } },
+			{ { { 0 },	"shdr-fs:draw-tex-outline",			a3shader_fragment,	1,{ A3_DEMO_FS"04-multipass/drawTexture_outline_fs4x.glsl" } } },
 			{ { { 0 },	"shdr-fs:draw-Phong-multi-shadow",	a3shader_fragment,	1,{ A3_DEMO_FS"04-multipass/drawPhong_multi_shadow_mrt_fs4x.glsl" } } },
 		}
 	};
