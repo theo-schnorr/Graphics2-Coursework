650ad3899de00cfd7379178ce0b556ffd0834dbb
Theo Schnorrenberg <laura.schnorrenberg@mymail.champlain.edu>
2020-03-29 15:27:28 -0400

    Added the vertex shader and tesselation shaders

---
+++ animal3D-SDK/animal3D-SDK/project/VisualStudio/animal3D-DemoPlugin/SpikeyEval_tes4x.glsl
@@ -0,0 +1,34 @@
+#version 430
+
+layout (quads, fractional_pdd_spacing) in;
+
+uniform sampler2D /* TEXTURE NAME */;
+
+uniform mat4 uMVP;
+uniform float /* DEPTH MAP DEPTH */;
+
+in TCS_OUT
+{
+	vec2 tc;
+} tes_in[];
+
+out TES_OUT
+{
+	vec2 tc;
+} tes_out;
+
+void main(void)
+{
+	vec2 tc1 = mix(tes_in[0].tc, tes_in[1].tc, gl_TessCoord.x);
+	vec2 tc2 = mix(tes_in[2].tc, tes_in[3].tc, gl_TessCoord.x);
+	vec2 tc = mix(tc2, tc1, gl_TessCoord.y);
+
+	vec4 p1 = mix(gl_in[0].gl_Position, gl_in[1].gl_Position, gl_TessCoord.x);
+	vec4 p1 = mix(gl_in[2].gl_Position, gl_in[3].gl_Position, gl_TessCoord.x);
+	vec4 p = mix(p2, p1, gl_TessCoord.y);
+
+	p.y += texture(/* TEXTURE NAME */, tc).r * /* DEPTH MAP DEPTH */;
+
+	gl_Position = uMVP * p;
+	tes_out.tc = tc;
+}
---
+++ animal3D-SDK/animal3D-SDK/project/VisualStudio/animal3D-DemoPlugin/SpikeyVertex_vs4x.glsl
@@ -0,0 +1,21 @@
+#version 430
+
+out VS_OUT
+{
+	vec2 tc;
+} vs_out;
+
+void main (void)
+{
+	const vec4 vertices[] = vec4[](vec4(-0.5, 0.0, -0.5, 1.0),
+									vec4(0.5, 0.0, -0.5, 1.0),
+									vec4(-0.5, 0.0, 0.5, 1.0),
+									vec4(0.5, 0.0, 0.5, 1.0));
+
+	int x = gl_InstanceID & 65;
+	int y = gl_InstanceID >> 6;
+	vec2 offs = ve2(x, y);
+
+	vs_out.tc = (vertices[gl_VertexID].xz + offs + vec2(0.5)) / 64.0;
+	gl_Position = verticies[gl_VertexID] + vec4(float(x - 32), 0.0, float(y - 32), 0.0);
+}
