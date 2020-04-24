a0b7e25209828e783e6981bc61c4c327e907a78e
Theo Schnorrenberg <laura.schnorrenberg@mymail.champlain.edu>
2020-04-24 16:02:52 -0400

    Trying out shaders

--- animal3D-SDK/animal3D-SDK/resource/glsl/4x/fs/02-shading/drawPhong_multi_fs4x.glsl
+++ animal3D-SDK/animal3D-SDK/resource/glsl/4x/fs/02-shading/drawPhong_multi_fs4x.glsl
@@ -32,54 +32,64 @@
 //	Note: test all data and inbound values before using them!
 
 in vec4 oMVNormie;
-in vec4 oVSPos; 
+in vec4 oVSPos;
 in vec4 oTexCoord;
 
+//uniform sampler2D uSmokeTex_dm;
+//uniform sampler2D uSmokeTex_sm;
 uniform sampler2D uTex_dm;
 uniform sampler2D uTex_sm;
 uniform int uLightCt;
-uniform float uLightSz;
-uniform float uLightSzInvSq;
 uniform vec4 uLightPos[4];
 uniform vec4 uLightCol[4];
+uniform double uTime;
 
 out vec4 rtFragColor;
-
-float diffuse(vec4 n, vec4 l, vec4 pos);
-float specular(vec4 viewer, vec4 pos, vec4 n, vec4 l, float shinyConstant);
-
-void main()
-{
-	// DUMMY OUTPUT: all fragments are OPAQUE GREEN
-	// phong = diffuse + specular + ambient;
-	vec4 normalizedN = normalize(oMVNormie);
-	vec4 phong  = vec4(0.0,0.0,0.0, 0.0);
-
-	for(int i = 0; i  < uLightCt; i++)
-	{
-		vec4 diffuse = diffuse(normalizedN, uLightPos[i], oVSPos)*uLightCol[i]* texture(uTex_dm, oTexCoord.xy);
-		vec4 specular = specular(vec4(0.0,0.0,0.0,1.0), oVSPos, normalizedN, uLightPos[i], 128.0) *  texture(uTex_sm, oTexCoord.xy);
-		phong += diffuse + specular;
-	}
-
-	rtFragColor = phong; //we didnt add ambient oh well it would probably super small anyway
-}
-
 
 float diffuse(vec4 n, vec4 l, vec4 pos)
 {
 	vec4 normalizedL = normalize(l-pos);
-	float dotPro = max(dot(n, normalizedL), 0.0);
-	
-	return dotPro;
+	return max(dot(n, normalizedL), 0.0);
 }
 
 float specular(vec4 viewerPos, vec4 pos, vec4 n, vec4 l, float shinyConstant)
 {
 	vec4 normalizedViewer = normalize(viewerPos - pos);
 	vec4 normalizedLight = normalize(l-pos);
-	vec4 normalizedReflection = reflect(-normalizedLight,n);
-	float specular = pow(max(dot(normalizedReflection, normalizedViewer), 0.0), shinyConstant);
-	
-	return specular;
+	vec4 normalizedReflection = reflect(-normalizedLight, n);
+	//return pow(max(dot(normalizedReflection, normalizedViewer), 0.0), shinyConstant); 
+	//i want to try something different, if it doesnt work we go to the old one
+	return pow(dot(normalizedReflection, normalizedViewer)/2.0+.5, shinyConstant);
+}
+
+vec4 phong(vec4 normie, vec4 viewSpacePos, sampler2D diffuseTex, sampler2D specularTex, vec4 texCoord)
+{
+	vec4 normalizedN = normalize(normie);
+	vec4 phong = vec4(0.0,0.0,0.0,0.0);
+
+	for(int i=0; i< uLightCt; i++)
+	{
+		vec4 diffuse = diffuse(normalizedN, uLightPos[i], viewSpacePos) * uLightCol[i]* texture(diffuseTex, texCoord.xy);
+		vec4 specular = specular(vec4(0.0,0.0,0.0,1.0), viewSpacePos, normalizedN, uLightPos[i], 128.0) * texture(specularTex, texCoord.xy);
+		phong += (diffuse + specular);
+	}
+	return phong;
+}
+
+vec4 colorCurveInterpolation(vec4 pink, vec4 yellow, vec4 cyan, double time)
+{
+	vec4 p1 = mix(pink, cyan, cos(float(time)));
+	vec4 p2 = mix(cyan, yellow, cos(float(time)));
+	return mix(p1, p2, cos(float(time))/2.0+.5);
+}
+
+void main()
+{
+	vec4 pink = vec4(1.0, 0.7, 1.0, 1.0);
+	vec4 cyan = vec4(0.7, 1.0, 1.0, 1.0);
+	vec4 yellow = vec4(1.0, 1.0, 0.7, 1.0);
+	//rtFragColor = pink;
+	//rtFragColor = colorCurveInterpolation(pink, yellow, cyan, uTime);
+	rtFragColor = phong(oMVNormie, oVSPos, uTex_dm, uTex_sm, oTexCoord) * colorCurveInterpolation(pink, yellow, cyan, uTime);
+	//rtFragColor = phong(oMVNormie, uLightCt, uLightPos, oVSPos, uLightCol, uSmokeTex_dm, uSmokeTex_sm, oTexCoord);
 }
--- animal3D-SDK/animal3D-SDK/resource/glsl/4x/vs/02-shading/passLightingData_transform_vs4x.glsl
+++ animal3D-SDK/animal3D-SDK/resource/glsl/4x/vs/02-shading/passLightingData_transform_vs4x.glsl
@@ -51,11 +51,27 @@ out vec4 oMVNormie;
 out vec4 oVSPos; 
 out vec4 oTexCoord;
 
-void main()
+out VS_OUT
 {
+	vec2 tc;
+} vs_out;
+
+void main (void)
+{
+	const vec4 vertices[] = vec4[](vec4(-0.5, 0.0, -0.5, 1.0),
+									vec4(0.5, 0.0, -0.5, 1.0),
+									vec4(-0.5, 0.0, 0.5, 1.0),
+									vec4(0.5, 0.0, 0.5, 1.0));
+
+	int x = gl_InstanceID & 65;
+	int y = gl_InstanceID >> 6;
+	vec2 offs = vec2(x, y);
+
 	oVSPos = uMV * aPosition;
-	oMVNormie = uMV_nrm * aNormal; 
-	oTexCoord = uAtlas * aTexCoord;
-	
+    oMVNormie = uMV_nrm * aNormal; 
+    oTexCoord = uAtlas * aTexCoord;
+
+	vs_out.tc = (vertices[gl_VertexID].xz + offs + vec2(0.5)) / 64.0;
+	//gl_Position = vertices[gl_VertexID] + vec4(float(x - 32), 0.0, float(y - 32), 0.0);
 	gl_Position = uP * uMV * aPosition;
 }
